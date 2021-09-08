4293adcc2667212f6d3d9f39443036b75ccdc3ad
g1otis <g1otis.dev@gmail.com>
2021-09-08 22:45:49 +0300

    * CustomerManagement.UnitTests.csproj: New libs for in memory db   tests
    * CustomerManagementContextTests.cs: Database schema test

---
+++ src/CRM.Services/CustomerManagement/CustomerManagement.UnitTests/Infrastructure/CustomerManagementContextTests.cs
@@ -0,0 +1,26 @@
+﻿using CustomerManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace CustomerManagement.UnitTests.Infrastructure
+{
+    public class CustomerManagementContextTests
+    {
+        private readonly CustomerManagementContext context;
+
+        public CustomerManagementContextTests()
+        {
+            var options = new DbContextOptionsBuilder<CustomerManagementContext>()
+                    .UseSqlite("Filename=Test.db")
+                    .Options;
+
+            context = new CustomerManagementContext(options);
+        }
+
+        [Fact]
+        public void EnsureCreated_Succeeds()
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
